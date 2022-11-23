872629d95fd6434cf28ad547fb37878ff311816c
Neobraingit <Neobraingit@github.com>
2022-11-23 19:32:44 +0100

    Listas

--- 04_listas.py
+++ 04_listas.py
@@ -4,4 +4,20 @@ print (len(my_list)) # Nos dice la longitud de la lista
 print (my_list)
 
 my_other_list = [1, 2, 3, 4, 5]
-print (type(my_other_list))
+print (type(my_other_list)) # Sabemos de qué tipo es el dato
+print (my_other_list[0]) # Hacemos slicing en las listas
+print (my_other_list[1:3]) # Otra vez slicing
+print (my_other_list[::-1]) # Reverse a la lista
+print (my_other_list.count(3)) # Nos dice cuantos objetos como el que le indiquemos hay
+print (my_other_list.pop()) # Nos muestra el último índice de la lista y lo borra
+print (my_other_list + my_list) # Concatenamos dos listas
+print (my_other_list.append('Caballo')) # Añadimos un elemento a la lista
+print (my_other_list)
+print (my_other_list.insert(1, 'Cochino')) # Nos pide un índice e insertaremos ahí el nuevo elemento
+print (my_other_list)
+
+
+
+
+
+
