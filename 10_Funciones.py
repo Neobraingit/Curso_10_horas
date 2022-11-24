306dce40f486978d56588e6352f7585a21aa9448
Neobraingit <Neobraingit@github.com>
2022-11-24 20:12:46 +0100

    Clases

--- 10_Funciones.py
+++ 10_Funciones.py
@@ -11,3 +11,15 @@ def mi_funcion2(num1, num2): # Declaramos la función con parámetros
 
 mi_funcion2(3, 5) # Ejecutamos la función con los parámetros requeridos
 
+def mi_funcion3(num1, num2):
+
+    return num1 + num2 # Esto no devuelve nada si no lo guardamos en una variable
+
+resultado = mi_funcion3(4, 5)
+print (resultado)
+
+def print_text(*text):
+    print (text)
+
+print_text('Marcos', 'Carmona', 'García') # Podemos pasar varios parámetros por el asterisco
+
